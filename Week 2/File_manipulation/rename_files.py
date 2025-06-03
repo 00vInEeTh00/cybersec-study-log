99a3ba3502a8d9ebf1bf728274e997961debd378
vineeth m <163656810+00vInEeTh00@users.noreply.github.com>
2025-06-03 10:55:58 +0530

    Add files via upload

---
+++ Week 2/File_manipulation/rename_files.py
@@ -0,0 +1,20 @@
+import os
+
+path = "files" #file stored folder
+files=os.listdir(path) #get all files list
+
+for index, filename in enumerate(files):
+
+    old_path=os.path.join(path,filename)
+    # Gets file path & name
+
+    new_name = f"document_{index+1}.txt"
+    #create new file name
+
+    new_path = os.path.join(path,new_name)
+    #joined path & new name
+
+    os.rename(old_path,new_path)
+    #change old path/name to new path/name
+
+print("All files are renamed successfully")
