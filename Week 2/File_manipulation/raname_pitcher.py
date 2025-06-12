d01fa46d82bd3ecb3b039fa4e239ad2a72ea08db
vineeth m <163656810+00vInEeTh00@users.noreply.github.com>
2025-06-12 16:43:42 +0530

    Add files via upload

---
+++ Week 2/File_manipulation/raname_pitcher.py
@@ -0,0 +1,32 @@
+# Rename only images in a folder
+
+import os
+
+path = "C:/Users/vinee/Desktop/files/bulk_rename/files"
+files = os.listdir(path)
+
+image_files =[]
+for file in files:
+    if file.endswith((".jpg",".img")):
+        image_files.append(file)
+
+for index,file_name in enumerate(image_files):
+    
+    if file_name.endswith((".jpg",".img")):
+
+        extension = file_name.split(".")[-1]
+        
+        old_path = os.path.join(path,file_name)
+        
+        new_name = f"photos_{index+1}.{extension}"
+        
+        new_path = os.path.join(path,new_name)
+        
+        os.rename(old_path,new_path)
+
+print("Renaming done...")
+
+
+
+
+
